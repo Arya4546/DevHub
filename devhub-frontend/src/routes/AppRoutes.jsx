f8dd7e986be889794081c5dc67867b60bbc753c0
Arya Deep Singh <singharya9693@gmail.com>
2025-06-29 20:25:16 +0530

    Update AppRoutes.jsx

--- devhub-frontend/src/routes/AppRoutes.jsx
+++ devhub-frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "../pages/Auth/Signup";
 import Login from "../pages/Auth/Login";
 import Feed from "../pages/Feed";
@@ -7,20 +7,65 @@ import Chat from "../pages/Chat";
 import Profile from "../pages/Profile";
 import NotFound from "../pages/NotFound";
 import PairUp from "../pages/PairUp";
+import PrivateRoute from "./PrivateRoute";
+
 export default function AppRoutes() {
+  const token = localStorage.getItem("devhub_token");
+
   return (
     <Routes>
-      <Route path="/" element={<Feed />} />
+      {/* If not logged in, show login; otherwise redirect to /feed */}
+      <Route
+        path="/"
+        element={
+          token ? <Navigate to="/feed" replace /> : <Navigate to="/login" replace />
+        }
+      />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/feed" element={<Feed />} />
-      <Route path="/bugs" element={<BugReports />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/profile/:id" element={<Profile />} />
-      <Route path="*" element={<NotFound />} />
-     
-      <Route path="/pairup" element={<PairUp />} />
 
+      {/* Protected routes */}
+      <Route
+        path="/feed"
+        element={
+          <PrivateRoute>
+            <Feed />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/bugs"
+        element={
+          <PrivateRoute>
+            <BugReports />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/chat"
+        element={
+          <PrivateRoute>
+            <Chat />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/profile/:id"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/pairup"
+        element={
+          <PrivateRoute>
+            <PairUp />
+          </PrivateRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
